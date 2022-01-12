1f75d3cffe567145a33723fd038f68cc256bc2db
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2022-01-12 16:27:28 -0300

    Cria os components Dropdown e Checkbox

---
+++ src/components/UI/Checkbox/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.scss';
+import prop_types from 'prop-types';
+
+export const Checkbox = ({ content, isChecked }) => {
+  return (
+    <label className="checkbox-container" htmlFor="">
+      <input type="checkbox" onClick={isChecked} />
+      <span>{content}</span>
+    </label>
+  )
+}
+
+Checkbox.propTypes = {
+  isChecked: prop_types.func,
+  content: prop_types.string.isRequired,
+}
---
+++ src/components/UI/Dropdown/index.jsx
@@ -0,0 +1,45 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import './styles.scss';
+
+/*eslint-disable*/
+export const Dropdown = ({ title, filter, onFilter }) => {
+  const [dropdownValue, setDropdownValue] = useState(title);
+  const dropdownRef = useRef();
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    onFilter(dropdownValue);
+  }, [dropdownValue])
+
+  const handleActive = () => setActive(!active);
+
+  const handleDropdownValue = e => {
+    let valor = dropdownValue;
+    setDropdownValue(e.target.textContent);
+    e.target.textContent = valor;
+    handleActive(!active);
+  };
+
+  const handleFilters = useCallback(() => {
+    return (
+      filter.map(currentFilter => (
+        <li key={currentFilter} onClick={e => handleDropdownValue(e)}>{currentFilter}</li>
+      ))
+    )
+  })
+
+  return (
+    <div onClick={handleActive} className={`menu-dropdown ${active ? "open" : ""}`}>
+      <span>{dropdownValue}</span>
+      <img src={process.env.PUBLIC_URL + '/Images/seta-baixo.svg'} alt="" />
+      <nav
+        className={`menu ${active ? "active" : ""}`}
+        ref={dropdownRef}
+      >
+        <ul>
+          {handleFilters()}
+        </ul>
+      </nav>
+    </div>
+  )
+}
